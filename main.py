57163a3fbf0647f85ea4523639bb9f0cbb4e45b4
Michael Mathena <mathenamr@hotmail.com>
2020-10-28 22:05:27 -0400

    practice

--- main.py
+++ main.py
@@ -5,3 +5,18 @@ if (x > 2): #comment
     print("Five is greater than 2!")
 print("Test")
 x = "Hello, World"
+x = "awesome"
+print("Python is " + x)
+
+a = "This is"
+b = "python."
+c = a + " " + b
+print(c)
+def myfunc(p):
+#    var2 = var1 + p
+    var1 = p
+    print(var1)
+
+var1 = "3"
+myfunc("cat")
+print(var1)
